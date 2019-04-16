c40343b8994a2d6dd9e8a060d9b8ef226c63ba94
Nem <nemx14@gmail.com>
2019-04-16 14:41:05 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PW: ");
+            string hashed_pw = SecurePasswordHasherHelper.Hash(Console.ReadLine());
+
+            //extract iteration and Base64 string
+            var splittedHashString = hashed_pw.Replace("$MYHASH$V1$", "").Split('$');
+            var iterations = int.Parse(splittedHashString[0]);
+            var base64Hash = splittedHashString[1];
+
+            //get hashbytes
+            var hashBytes = Convert.FromBase64String(base64Hash);
+
+            //get salt
+            var salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            //get hash
+            var hash = new byte[20];
+            Array.Copy(hashBytes, salt.Length, hash, 0, 20);
+
+            Console.WriteLine("Hashed PW: " + hashed_pw);
+            Console.WriteLine("Salt: " + Convert.ToBase64String(salt));
+            Console.WriteLine("Hash: " + Convert.ToBase64String(hash));
+
+            if (SecurePasswordHasherHelper.Verify(Console.ReadLine(), hashed_pw))
+            {
+                Console.WriteLine("Password Verified");
+                Console.ReadKey();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
